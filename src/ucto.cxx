d73514dde463cdd9435d9940fb1c5c17752e5225
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2014-08-21 15:23:39 +0000

    switched to TiCC CommandLine stuff
    git-svn-id: https://ilk.uvt.nl/svn/sources/ucto/trunk@17544 12f355fe-0486-481a-ad91-c297ab22b4e3

--- src/ucto.cxx
+++ src/ucto.cxx
@@ -27,11 +27,11 @@
 */
 #include <cstdlib>
 #include <cstring>
-#include <getopt.h>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include "libfolia/document.h"
+#include "ticcutils/CommandLine.h"
 #include "ucto/tokenize.h"
 #include <unistd.h>
 
@@ -93,59 +93,108 @@ int main( int argc, char *argv[] ){
   string ofile;
   string c_file;
   string L_file;
-
-  static struct option longOpts[] = { { "passthru", 0, 0, 1 },
-				      { "id", 1, 0, 2 },
-				      { "textclass", 1, 0, 3 },
-				      { "inputclass", 1, 0, 4 },
-				      { "outputclass", 1, 0, 5 },
-				      { 0,0,0,0} };
-
-  int opt;
-  int longOpt;
   bool passThru = false;
+
   try {
-    while ((opt = getopt_long( argc, argv,
-			       "d:e:fhlPQunmN:vVSL:c:s:x:FX",
-			       longOpts, &longOpt )) != -1) {
-      switch (opt)
-	{
-	case 1: passThru = true; break;
-	case 'd': debug = stringTo<int>(optarg); break;
-	case 'e': inputEncoding = optarg; break;
-	case 'f': dofiltering = false; break;
-	case 'F': xmlin = true; break;
-	case 'P': paragraphdetection = false; break;
-	case 'Q': quotedetection = true; break;
-	case 'c': c_file = optarg; break;
-	case 's': eosmarker = optarg; break;
-	case 'S': splitsentences = false; break;
-	case 'L': L_file = string("tokconfig-") + string(optarg); break;
-	case 'u': touppercase = true; break;
-	case 'l': tolowercase = true; break;
-	case 'n': sentenceperlineoutput = true; break;
-	case 'm': sentenceperlineinput = true; break;
-	case 'N': normalization = optarg; break;
-	case 'v': verbose = true; break;
-	case 'V': cout << "Ucto - Unicode Tokenizer - version " << Version() << endl << "(c) ILK 2009 - 2014, Induction of Linguistic Knowledge Research Group, Tilburg University" << endl << "Licensed under the GNU General Public License v3" << endl;
-	  cout << "based on [" << folia::VersionName() << "]" << endl;
-	  return EXIT_SUCCESS;
-	case 'x': xmlout = true; docid = optarg; break;
-	case 'X': xmlout = true; break;
-	case 2: docid = optarg; break;
-	case 3: inputclass = optarg;
-	  cerr << "--textclass is deprecated!. use --inputclass instead!" << endl;
-	  break;
-	case 4: inputclass = optarg; break;
-	case 5: outputclass = optarg; break;
-	default: usage(); return EXIT_SUCCESS;
-	}
+    TiCC::CL_Options Opts( "d:e:fhlPQunmN:vVSL:c:s:x:FX",
+			   "passthru,textclass:,inputclass:,outputclass:,id:");
+    Opts.init(argc, argv );
+    string value;
+    bool mood;
+    if ( Opts.pull('d', value, mood) ){
+      debug = stringTo<int>(value);
+    }
+    if ( Opts.pull('e', value, mood) ){
+      inputEncoding = value;
+    }
+    if ( Opts.pull('f', value, mood) ){
+      dofiltering = false;
+    }
+    if ( Opts.pull('F', value, mood) ){
+      xmlin = true;
+    }
+    if ( Opts.pull('P', value, mood) ){
+      paragraphdetection = false;
+    }
+    if ( Opts.pull('Q', value, mood) ){
+      quotedetection = true;
+    }
+    if ( Opts.pull('c', value, mood) ){
+      c_file = value;
+    }
+    if ( Opts.pull('s', value, mood) ){
+      eosmarker = value;
+    }
+    if ( Opts.pull('S', value, mood) ){
+      splitsentences = true;
+    }
+    if ( Opts.pull('L', value, mood) ){
+      L_file = string("tokconfig-") + string(value);
+    }
+    if ( Opts.pull('u', value, mood) ){
+      touppercase = true;
+    }
+    if ( Opts.pull('l', value, mood) ){
+      tolowercase = true;
+    }
+    if ( Opts.pull('n', value, mood) ){
+      sentenceperlineoutput = true;
+    }
+    if ( Opts.pull('m', value, mood) ){
+      sentenceperlineinput = true;
+    }
+    if ( Opts.pull('N', value, mood) ){
+      normalization = value;
+    }
+    if ( Opts.pull('v', value, mood) ){
+      verbose = true;
+    }
+    if ( Opts.pull('V', value, mood) ){
+      cout << "Ucto - Unicode Tokenizer - version " << Version() << endl
+	   << "(c) ILK 2009 - 2014, Induction of Linguistic Knowledge Research Group, Tilburg University" << endl
+	   << "Licensed under the GNU General Public License v3" << endl;
+      cout << "based on [" << folia::VersionName() << "]" << endl;
+      return EXIT_SUCCESS;
+    }
+    if ( Opts.pull('x', value, mood) ){
+      xmlout = true;
+      docid = value;
+    }
+    if ( Opts.pull('X', value, mood) ){
+      xmlout = true;
+    }
+    if ( Opts.pull("passthru", value) ){
+      passThru = true;
+    }
+    if ( Opts.pull("id", value ) ){
+      docid = value;
+    }
+    if ( Opts.pull("textclass", value ) ){
+      cerr << "--textclass is deprecated!. use --inputclass instead!" << endl;
+      inputclass = value;
+    }
+    if ( Opts.pull("inputclass", value ) ){
+      inputclass = value;
+    }
+    if ( Opts.pull("outputclass", value ) ){
+      outputclass = value;
+    }
+    if ( !Opts.empty() ){
+      string tomany = Opts.toString();
+      throw invalid_argument( tomany );
+    }
+    vector<string> files = Opts.getMassOpts();
+    if ( files.size() > 0 ){
+      ifile = files[0];
+    }
+    if ( files.size() > 1 ){
+      ofile = files[1];
     }
   }
-  catch ( exception &e ){
-    cerr << "Error in option '" << char(opt) << "' : " << e.what() << endl << endl;
+  catch( const TiCC::OptionError& e ){
+    cerr << "ucto: " << e.what() << endl;
     usage();
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
 
   if ( !passThru ){
@@ -164,9 +213,6 @@ int main( int argc, char *argv[] ){
       return EXIT_FAILURE;
     }
   }
-
-  if (optind < argc) ifile = argv[optind++];
-  if (optind < argc) ofile = argv[optind++];
 
   if ((!ifile.empty()) && (ifile == ofile)) {
     cerr << "Error: Output file equals input file! Courageously refusing to start..."  << endl;
