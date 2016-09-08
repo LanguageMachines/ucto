06ddc6117757ba336bf2930410366d94e1a3a3a4
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2016-09-08 14:33:10 +0200

    modified test.py so that ucto finds the data automagicly, not using -c.

--- tests/test.py
+++ tests/test.py
@@ -73,15 +73,15 @@ for f in glob.glob(mask):
     if os.path.isfile(id + '.' + lang + '.tok.V'):
         reffile = id + '.' + lang + '.tok.V'
         outputfile = 'testoutput/' + id + '.' + lang + '.tok.V'
-        cmd = VG+'../src/.libs/ucto -Q -v -c ../../uctodata/config/tokconfig-' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err'
+        cmd = VG+'../src/.libs/ucto -Q -v -L' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err'
     elif os.path.isfile(id + '.' + lang + '.tok'):
         reffile = id + '.' + lang + '.tok'
         outputfile = 'testoutput/' + id + '.' + lang + '.tok'
-        cmd = VG+'../src/.libs/ucto -Q -c ../../uctodata/config/tokconfig-' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err'
+        cmd = VG+'../src/.libs/ucto -Q -L' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err'
     elif os.path.isfile(id + '.' + lang + '.xml'):
         reffile = id + '.' + lang + '.xml'
         outputfile = 'testoutput/' + id + '.' + lang + '.xml'
-        cmd = VG+'../src/.libs/ucto --id="test" -Q -X -c ../../uctodata/config/tokconfig-' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err'
+        cmd = VG+'../src/.libs/ucto --id="test" -Q -X -L' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err'
     else:
         print color_text("MISSING", 'RED', True)
         exitcode += 1
@@ -114,9 +114,9 @@ for f in glob.glob(mask):
             log += "testoutput/" + id + '.' + lang + '.diff\n'
             log += "testoutput/" + id + '.' + lang + '.err\n'
             if outputfile[-4:] != ".xml":
-                os.system('../src/.libs/ucto -Q -d ' + str(DEBUGLEVEL) + ' -v -c ../../uctodata//config/tokconfig-' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err')
+                os.system('../src/.libs/ucto -Q -d ' + str(DEBUGLEVEL) + ' -v -L' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err')
             else:
-                os.system('../src/.libs/ucto --id="test" -X -Q -d ' + str(DEBUGLEVEL) + ' -v -c ../../uctodata/config/tokconfig-' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err')
+                os.system('../src/.libs/ucto --id="test" -X -Q -d ' + str(DEBUGLEVEL) + ' -v -L' + lang + ' ' + f + ' ' + outputfile + ' 2> testoutput/' + id + '.' + lang + '.err')
 
 if log:
     print "--------------------------"
