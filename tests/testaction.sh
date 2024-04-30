98f0d4b869b83ee2a2b0e5248705ecf565e03b74
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2024-04-30 10:57:49 +0200

    still chasing my own tail

--- tests/testaction.sh
+++ tests/testaction.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo "Running testaction script!"
-./testall.sh
+#./testall.sh
+sh -x testone.sh testfolia
+echo "ERRROR FILE"
+cat testoutput/testfolia.err
+echo "TEMP FILE"
+cat testoutput/testfolia.tmp
+echo "DIFF FILE"
+cat testoutput/testfolia.diff
 TEST_STAT=$?
 echo $TEST_STAT > status.tmp
 exit 0
